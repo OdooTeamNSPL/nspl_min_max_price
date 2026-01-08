28ab07a9a05297becbdb6c4a7d595284b6bd01a2
Team Odoo NSPL <odoo_team@namahsoftech.com>
2026-01-08 17:33:11 +0530

    Delete nspl_min_max_price directory

--- nspl_min_max_price/models/product_template.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-    min_price = fields.Float(string="Minimum Price")
-    max_price = fields.Float(string="Maximum Price")
